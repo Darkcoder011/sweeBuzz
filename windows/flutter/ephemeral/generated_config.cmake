b8ca00c863cb45b5c91cfc197024ded3f4e69e0b
Abhijit Dengale <107787867+Darkcoder011@users.noreply.github.com>
2023-08-17 08:50:02 +0530

    new upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Abhijit\\Documents\\flutter_windows_3.7.12-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Abhijit\\Desktop\\MyProject\\assitant" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Abhijit\\Documents\\flutter_windows_3.7.12-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Abhijit\\Desktop\\MyProject\\assitant"
+  "FLUTTER_ROOT=C:\\Users\\Abhijit\\Documents\\flutter_windows_3.7.12-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Abhijit\\Desktop\\MyProject\\assitant\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Abhijit\\Desktop\\MyProject\\assitant"
+  "FLUTTER_TARGET=C:\\Users\\Abhijit\\Desktop\\MyProject\\assitant\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lMTRkYjY4YTg2MTk5YTlmYTVmZDk2MjhkOGUyOTBhYWZjMGY2OTE3Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Abhijit\\Desktop\\MyProject\\assitant\\.dart_tool\\package_config.json"
+)
